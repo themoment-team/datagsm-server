b25c7868ec958838c7d5bb2a090a381c914bc676
snowykte0426 <snowykte0426@naver.com>
2025-09-11 23:01:50 +0900

    update(global): Ktlint 포맷팅 문제 해결

--- src/main/kotlin/team/themoment/datagsm/DatagsmApplication.kt
+++ src/main/kotlin/team/themoment/datagsm/DatagsmApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class DatagsmApplication
 
 fun main(args: Array<String>) {
-	runApplication<DatagsmApplication>(*args)
+    runApplication<DatagsmApplication>(*args)
 }
