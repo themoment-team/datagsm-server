4d408390f73c2630a34a5e3261a32204b704aef3
snowykte0426 <snowykte0426@naver.com>
2025-09-14 22:35:55 +0900

    add(club): 동아리 엔티티 정의

---
+++ src/main/kotlin/team/themoment/datagsm/domain/club/entity/ClubJpaEntity.kt
@@ -0,0 +1,32 @@
+package team.themoment.datagsm.domain.club.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.DynamicUpdate
+import team.themoment.datagsm.domain.club.entity.constant.ClubType
+
+@Table(name = "tb_club")
+@Entity
+@DynamicUpdate
+class ClubJpaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "club_id")
+    var clubId: Long? = null
+
+    @Column(name = "club_name", nullable = false, unique = true, length = 50)
+    lateinit var clubName: String
+
+    @Column(name = "club_description", nullable = false, columnDefinition = "TEXT")
+    lateinit var clubDescription: String
+
+    @Column(name = "club_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    lateinit var clubType: ClubType
+}
---
+++ src/main/kotlin/team/themoment/datagsm/domain/club/entity/constant/ClubType.kt
@@ -0,0 +1,7 @@
+package team.themoment.datagsm.domain.club.entity.constant
+
+enum class ClubType {
+    MAJOR_CLUB,
+    JOB_CLUB,
+    AUTONOMOUS_CLUB
+}
