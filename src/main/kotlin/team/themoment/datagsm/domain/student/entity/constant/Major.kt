0d6872b7f70d008ae4c2fcf80534f339d81c7137
snowykte0426 <snowykte0426@naver.com>
2025-09-23 14:24:39 +0900

    add(student): 반 정보를 입력받아 학과를 추론하는 메서드 추가

--- src/main/kotlin/team/themoment/datagsm/domain/student/entity/constant/Major.kt
+++ src/main/kotlin/team/themoment/datagsm/domain/student/entity/constant/Major.kt
@@ -4,4 +4,15 @@ enum class Major {
     SW_DEVELOPMENT,
     SMART_IOT,
     AI,
+    ;
+
+    companion object {
+        fun fromGrade(grade: Int): Major? =
+            when (grade) {
+                1, 2 -> SW_DEVELOPMENT
+                3 -> SMART_IOT
+                4 -> AI
+                else -> null
+            }
+    }
 }
