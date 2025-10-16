b730b9d39945bf78ac7e990ac9eed9d8e52646a2
snowykte0426 <snowykte0426@naver.com>
2025-10-16 12:11:11 +0900

    update(auth): Google OAuth 인증 엔드포인트 구현

--- src/main/kotlin/team/themoment/datagsm/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/team/themoment/datagsm/domain/auth/controller/AuthController.kt
@@ -1,9 +1,24 @@
-package team.themoment.datagsm.domain.auth.controller;
+package team.themoment.datagsm.domain.auth.controller
 
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import team.themoment.datagsm.domain.auth.dto.OAuthCodeReqDto
+import team.themoment.datagsm.domain.auth.dto.TokenResDto
+import team.themoment.datagsm.domain.auth.service.GoogleOAuthService
 
 @RestController
 @RequestMapping("/v1/auth")
-class AuthController {
+class AuthController(
+    private val googleOAuthService: GoogleOAuthService
+) {
+
+    @PostMapping
+    fun authenticateWithGoogle(
+        @RequestBody @Valid reqDto: OAuthCodeReqDto
+    ): TokenResDto {
+        return googleOAuthService.authenticate(reqDto.code)
+    }
 }
