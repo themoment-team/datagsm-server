6798650e156cb354fef9364247c78f6460c1fcfb
snowykte0426 <snowykte0426@naver.com>
2025-10-16 12:09:23 +0900

    update(thirdparty): Feign 설정 및 에러 디코더 구현

--- src/main/kotlin/team/themoment/datagsm/global/thirdparty/feign/config/FeignConfig.kt
+++ src/main/kotlin/team/themoment/datagsm/global/thirdparty/feign/config/FeignConfig.kt
@@ -1,4 +1,16 @@
-package team.themoment.datagsm.global.thirdparty.feign.config;
+package team.themoment.datagsm.global.thirdparty.feign.config
 
-public class FeignConfig {
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import team.themoment.datagsm.global.thirdparty.feign.error.FeignErrorDecoder
+
+@Configuration
+@EnableFeignClients(basePackages = ["team.themoment.datagsm"])
+class FeignConfig {
+
+    @Bean
+    fun feignErrorDecoder(): FeignErrorDecoder {
+        return FeignErrorDecoder()
+    }
 }
