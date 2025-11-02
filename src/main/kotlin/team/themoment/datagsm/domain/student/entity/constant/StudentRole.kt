e94ff7310d7084eb4369a11d69d7716cca6ce1ef
snowykte0426 <snowykte0426@naver.com>
2025-11-03 08:05:22 +0900

    docs(global): 학생 역할과 계정 권한 Enum 클래스에 주석 추가

--- src/main/kotlin/team/themoment/datagsm/domain/student/entity/constant/StudentRole.kt
+++ src/main/kotlin/team/themoment/datagsm/domain/student/entity/constant/StudentRole.kt
@@ -1,5 +1,11 @@
 package team.themoment.datagsm.domain.student.entity.constant
 
+import team.themoment.datagsm.domain.account.entity.constant.AccountRole
+
+/**
+ * 인증/인가와 관련 없이 학생의 역할을 정의하는 열거형 클래스입니다.
+ * @see AccountRole
+ */
 enum class StudentRole {
     STUDENT_COUNCIL,
     DORMITORY_MANAGER,
