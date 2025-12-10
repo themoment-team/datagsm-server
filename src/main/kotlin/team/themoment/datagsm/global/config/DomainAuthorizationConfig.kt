cd1e33c86376135a8fd9acff3ed3e2cee147cc2d
snowykte0426 <snowykte0426@naver.com>
2025-12-11 00:49:52 +0900

    update(global): 엔드포인트별 접근권한 설정 추가

--- src/main/kotlin/team/themoment/datagsm/global/config/DomainAuthorizationConfig.kt
+++ src/main/kotlin/team/themoment/datagsm/global/config/DomainAuthorizationConfig.kt
@@ -3,6 +3,7 @@ package team.themoment.datagsm.global.config
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
 import org.springframework.stereotype.Component
+import team.themoment.datagsm.domain.account.entity.constant.AccountRole
 
 @Component
 class DomainAuthorizationConfig {
@@ -13,7 +14,9 @@ class DomainAuthorizationConfig {
             .requestMatchers("/v1/auth/google", "/v1/auth/refresh")
             .permitAll()
             .requestMatchers("/v1/auth/api-key/**")
-            .authenticated()
+            .hasAnyAuthority(AccountRole.USER.authority, AccountRole.ADMIN.authority, AccountRole.ROOT.authority)
+            .requestMatchers("/v1/club/**", "/v1/project/**", "/v1/students/**")
+            .hasAnyAuthority(AccountRole.ADMIN.authority, AccountRole.ROOT.authority, AccountRole.API_KEY_USER.authority)
             .anyRequest()
             .authenticated()
     }
