9c974aeaea4f41a7cdaf0afdc543668daff474cc
snowykte0426 <snowykte0426@naver.com>
2025-12-11 10:51:15 +0900

    update(global): RequireScope 어노테이션 파라미터를 ApiScope enum으로 변경

--- src/main/kotlin/team/themoment/datagsm/global/security/annotation/RequireScope.kt
+++ src/main/kotlin/team/themoment/datagsm/global/security/annotation/RequireScope.kt
@@ -1,10 +1,11 @@
 package team.themoment.datagsm.global.security.annotation
 
 import org.springframework.security.access.prepost.PreAuthorize
+import team.themoment.datagsm.domain.auth.entity.constant.ApiScope
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@PreAuthorize("@scopeChecker.hasScope(authentication, '{scope}')")
+@PreAuthorize("@scopeChecker.hasScope(authentication, '{scope.scope}')")
 annotation class RequireScope(
-    val scope: String,
+    val scope: ApiScope,
 )
