56e728fa4e5eaedc679ee22f86f047dfb7203ab0
snowykte0426 <snowykte0426@naver.com>
2025-12-11 19:52:07 +0900

    refactor(auth): API 키 서비스 통합 및 서비스 계층 권한 검증 추가

--- src/main/kotlin/team/themoment/datagsm/domain/auth/service/CreateAdminApiKeyService.kt
+++
@@ -1,8 +0,0 @@
-package team.themoment.datagsm.domain.auth.service
-
-import team.themoment.datagsm.domain.auth.dto.request.CreateApiKeyReqDto
-import team.themoment.datagsm.domain.auth.dto.response.ApiKeyResDto
-
-interface CreateAdminApiKeyService {
-    fun execute(reqDto: CreateApiKeyReqDto): ApiKeyResDto
-}
