ec7bd446577f4d11bee52b907e95266af678a42f
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-12-13 15:28:27 +0900

    fix(client): 잘못된 Converter 구현을 objectMapper를 사용하도록 변경

--- src/main/kotlin/team/themoment/datagsm/global/common/converter/StringListConverter.kt
+++ src/main/kotlin/team/themoment/datagsm/global/common/converter/StringListConverter.kt
@@ -1,18 +1,20 @@
 package team.themoment.datagsm.global.common.converter
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.persistence.AttributeConverter
 import jakarta.persistence.Converter
 
 @Converter
 class StringListConverter : AttributeConverter<List<String>, String> {
-    private val delimiter = ","
+    private val objectMapper = ObjectMapper()
 
-    override fun convertToDatabaseColumn(attribute: List<String>): String = attribute.joinToString(delimiter)
+    override fun convertToDatabaseColumn(attribute: List<String>): String = objectMapper.writeValueAsString(attribute)
 
     override fun convertToEntityAttribute(dbData: String): List<String> =
         if (dbData.isEmpty()) {
             emptyList()
         } else {
-            dbData.split(delimiter)
+            objectMapper.readValue(dbData, object : TypeReference<List<String>>() {})
         }
 }
