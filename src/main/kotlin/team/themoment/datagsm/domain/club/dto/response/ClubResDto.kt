57a7473371f00f4bf7a38e97f21919d675bdc090
snowykte0426 <snowykte0426@naver.com>
2025-12-22 18:37:14 +0900

    add(club): 동아리 부장 및 부원 정보를 포함하도록 DTO 수정

--- src/main/kotlin/team/themoment/datagsm/domain/club/dto/response/ClubResDto.kt
+++ src/main/kotlin/team/themoment/datagsm/domain/club/dto/response/ClubResDto.kt
@@ -1,6 +1,7 @@
 package team.themoment.datagsm.domain.club.dto.response
 
 import io.swagger.v3.oas.annotations.media.Schema
+import team.themoment.datagsm.domain.club.dto.internal.ParticipantInfoDto
 import team.themoment.datagsm.domain.club.entity.constant.ClubType
 
 data class ClubResDto(
@@ -10,4 +11,8 @@ data class ClubResDto(
     val name: String,
     @param:Schema(description = "동아리 종류", example = "MAJOR_CLUB")
     val type: ClubType,
+    @param:Schema(description = "동아리 부장")
+    val leader: ParticipantInfoDto,
+    @param:Schema(description = "동아리 부원 목록")
+    val participants: List<ParticipantInfoDto>,
 )
