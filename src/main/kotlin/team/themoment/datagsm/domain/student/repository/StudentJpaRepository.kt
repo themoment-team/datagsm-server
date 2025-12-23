ddacb00aa559d943e7cc933bf9c5f0ad4ecc495c
snowykte0426 <snowykte0426@naver.com>
2025-12-23 23:56:20 +0900

    update(student): 미사용 쿼리 메서드 제거

--- src/main/kotlin/team/themoment/datagsm/domain/student/repository/StudentJpaRepository.kt
+++ src/main/kotlin/team/themoment/datagsm/domain/student/repository/StudentJpaRepository.kt
@@ -2,7 +2,6 @@ package team.themoment.datagsm.domain.student.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import team.themoment.datagsm.domain.club.entity.ClubJpaEntity
 import team.themoment.datagsm.domain.student.entity.StudentJpaEntity
 import team.themoment.datagsm.domain.student.repository.custom.StudentJpaCustomRepository
@@ -12,34 +11,6 @@ interface StudentJpaRepository :
     JpaRepository<StudentJpaEntity, Long>,
     StudentJpaCustomRepository {
     fun findByEmail(email: String): Optional<StudentJpaEntity>
-
-    @Query(
-        """
-    SELECT t FROM StudentJpaEntity t
-    LEFT JOIN FETCH t.majorClub
-    LEFT JOIN FETCH t.jobClub
-    LEFT JOIN FETCH t.autonomousClub
-    WHERE t.studentNumber.studentGrade = :grade
-    ORDER BY (
-        t.studentNumber.studentGrade * 1000 +
-        t.studentNumber.studentClass * 100 +
-        t.studentNumber.studentNumber
-    ) ASC
-""",
-    )
-    fun findStudentsByGrade(grade: Int): List<StudentJpaEntity>
-
-    @Query(
-        """
-        SELECT s FROM StudentJpaEntity s
-        WHERE (s.studentNumber.studentGrade * 1000 +
-        s.studentNumber.studentClass * 100 +
-        s.studentNumber.studentNumber) IN :studentNumbers
-    """,
-    )
-    fun findAllByStudentNumberIn(
-        @Param("studentNumbers") studentNumbers: List<Int>,
-    ): List<StudentJpaEntity>
 
     fun findByMajorClub(club: ClubJpaEntity): List<StudentJpaEntity>
 
