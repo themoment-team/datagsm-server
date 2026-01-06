4e6f8b61891d7d145763d45d499c5825cb7c26f1
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-01-06 17:20:35 +0900

    docs(account,oauth): DTO Swagger 문서화

--- src/main/kotlin/team/themoment/datagsm/domain/account/dto/request/SendEmailReqDto.kt
+++ src/main/kotlin/team/themoment/datagsm/domain/account/dto/request/SendEmailReqDto.kt
@@ -1,5 +1,14 @@
 package team.themoment.datagsm.domain.account.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class SendEmailReqDto(
+    @field:NotBlank(message = "이메일은 필수입니다.")
+    @field:Email(message = "유효한 이메일 형식이어야 합니다.")
+    @field:Size(max = 50)
+    @param:Schema(description = "인증 코드를 받을 이메일", example = "user@gsm.hs.kr", maxLength = 50)
     val email: String,
 )
