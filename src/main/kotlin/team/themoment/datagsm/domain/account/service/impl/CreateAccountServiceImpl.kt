e29a6ada471c83a69b5058a060a1a62b10985f5e
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-01-06 13:57:01 +0900

    fix(account): 계정 생성시 role 기본값이 초기화되지 않던 문제 해결

--- src/main/kotlin/team/themoment/datagsm/domain/account/service/impl/CreateAccountServiceImpl.kt
+++ src/main/kotlin/team/themoment/datagsm/domain/account/service/impl/CreateAccountServiceImpl.kt
@@ -1,16 +1,19 @@
 package team.themoment.datagsm.domain.account.service.impl
 
+import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.themoment.datagsm.domain.account.dto.request.CreateAccountReqDto
 import team.themoment.datagsm.domain.account.dto.request.EmailCodeReqDto
 import team.themoment.datagsm.domain.account.entity.AccountJpaEntity
+import team.themoment.datagsm.domain.account.entity.constant.AccountRole
 import team.themoment.datagsm.domain.account.repository.AccountJpaRepository
 import team.themoment.datagsm.domain.account.repository.EmailCodeRedisRepository
 import team.themoment.datagsm.domain.account.service.CheckEmailService
 import team.themoment.datagsm.domain.account.service.CreateAccountService
 import team.themoment.datagsm.domain.student.repository.StudentJpaRepository
+import team.themoment.datagsm.global.exception.error.ExpectedException
 
 @Service
 @Transactional
@@ -27,6 +30,7 @@ class CreateAccountServiceImpl(
             AccountJpaEntity.create(reqDto.email).apply {
                 password = passwordEncoder.encode(reqDto.password)
                 student = studentJpaRepository.findByEmail(reqDto.email).orElse(null)
+                role = AccountRole.USER
             }
         return accountJpaRepository.save(newAccount)
     }
