a4b3f96d7cce292147039259c2d58ba0d0241920
snowykte0426 <snowykte0426@naver.com>
2026-01-08 22:36:22 +0900

    update(global): Java 25 및 Spring Boot 4.0.0 마이그레이션 및 의존성 업데이트

--- buildSrc/src/main/kotlin/dependency/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependency/Dependencies.kt
@@ -19,7 +19,7 @@ object Dependencies {
     const val SPRING_WEB = "org.springframework.boot:spring-boot-starter-web"
     const val SPRING_SECURITY = "org.springframework.boot:spring-boot-starter-security"
     const val SPRING_VALIDATION = "org.springframework.boot:spring-boot-starter-validation"
-    const val SPRING_AOP = "org.springframework.boot:spring-boot-starter-aop"
+    const val SPRING_AOP = "org.springframework.boot:spring-boot-starter-aop:${DependencyVersions.SPRING_AOP_VERSION}"
     const val SPRINT_MAIL = "org.springframework.boot:spring-boot-starter-mail"
 
     // Spring Data
--- buildSrc/src/main/kotlin/dependency/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/dependency/DependencyVersions.kt
@@ -1,26 +1,28 @@
 package dependency
 
 object DependencyVersions {
-    const val KOTEST_VERSION = "6.0.1"
-    const val MOCKK_VERSION = "1.14.5"
+    const val SPRING_AOP_VERSION = "4.0.0-M2"
+
+    const val KOTEST_VERSION = "6.0.7"
+    const val MOCKK_VERSION = "1.14.7"
 
     const val JJWT_VERSION = "0.13.0"
 
-    const val QUERY_DSL_VERSION = "7.0"
+    const val QUERY_DSL_VERSION = "7.1"
 
-    const val SWAGGER_VERSION = "2.8.11"
+    const val SWAGGER_VERSION = "3.0.1"
 
     const val LOGBACK_AWS_APPENDER_VERSION = "1.6.0"
 
-    const val SPRING_CLOUD_VERSION = "2025.0.0"
-    const val AWS_SDK_VERSION = "2.29.45"
+    const val SPRING_CLOUD_VERSION = "2025.1.0"
+    const val AWS_SDK_VERSION = "2.41.2"
 
     const val PEANUT_BUTTER_VERSION = "1.4.1"
     const val THE_MOMENT_THE_SDK_VERSION = "1.0-RC1"
 
     const val KOTLIN_COROUTINES_VERSION = "1.10.2"
 
-    const val BUCKET4J_VERSION = "8.15.0"
+    const val BUCKET4J_VERSION = "8.16.0"
 
     const val POI_VERSION = "5.5.1"
 }
