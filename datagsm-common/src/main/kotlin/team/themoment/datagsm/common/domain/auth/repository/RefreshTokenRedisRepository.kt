914fb88e5731d7780027643dc7a8e2fcdfdf4f68
snowykte0426 <snowykte0426@naver.com>
2026-01-10 01:40:47 +0900

    fix(common): 누락된 RefreshTokenRedisRepository 복구

---
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/auth/repository/RefreshTokenRedisRepository.kt
@@ -0,0 +1,11 @@
+package team.themoment.datagsm.common.domain.auth.repository
+
+import org.springframework.data.repository.CrudRepository
+import team.themoment.datagsm.common.domain.account.RefreshTokenRedisEntity
+import java.util.Optional
+
+interface RefreshTokenRedisRepository : CrudRepository<RefreshTokenRedisEntity, String> {
+    fun findByEmail(email: String): Optional<RefreshTokenRedisEntity>
+
+    fun deleteByEmail(email: String)
+}
