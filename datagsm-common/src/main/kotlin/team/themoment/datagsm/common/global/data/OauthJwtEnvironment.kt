1a1f3f8ecf163830df762058a54c506bd71f950b
snowykte0426 <snowykte0426@naver.com>
2026-01-10 04:07:30 +0900

    refactor(oauth): OauthJwtEnvironment prefix 변경 및 프로퍼티명 간소화

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/global/data/OauthJwtEnvironment.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/global/data/OauthJwtEnvironment.kt
@@ -2,9 +2,9 @@ package team.themoment.datagsm.common.global.data
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "spring.security.jwt")
+@ConfigurationProperties(prefix = "spring.security.oauth-jwt")
 data class OauthJwtEnvironment(
     val secret: String,
-    val oauthAccessTokenExpiration: Long,
-    val oauthRefreshTokenExpiration: Long,
+    val accessTokenExpiration: Long,
+    val refreshTokenExpiration: Long,
 )
