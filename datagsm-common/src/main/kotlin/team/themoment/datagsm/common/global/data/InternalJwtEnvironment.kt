238760d16937f646a18986af1ebce12af5823ccd
snowykte0426 <snowykte0426@naver.com>
2026-01-10 04:07:21 +0900

    refactor(auth): InternalJwtEnvironment prefix를 spring.security.internal-jwt로 변경

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/global/data/InternalJwtEnvironment.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/global/data/InternalJwtEnvironment.kt
@@ -2,7 +2,7 @@ package team.themoment.datagsm.common.global.data
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "spring.security.jwt")
+@ConfigurationProperties(prefix = "spring.security.internal-jwt")
 data class InternalJwtEnvironment(
     val secret: String,
     val accessTokenExpiration: Long,
