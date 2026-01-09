f3b33102e5bcf18d0ae8323bf5f56df5c8f397c0
snowykte0426 <snowykte0426@naver.com>
2026-01-10 02:17:16 +0900

    fix(global): 프로젝트 내 미사용 클래스 및 DTO 정리

--- datagsm-resource/src/main/kotlin/team/themoment/datagsm/resource/global/security/dto/RateLimitConsumeResult.kt
+++
@@ -1,7 +0,0 @@
-package team.themoment.datagsm.resource.global.security.dto
-
-data class RateLimitConsumeResult(
-    val consumed: Boolean,
-    val remainingTokens: Long,
-    val secondsToWaitForRefill: Long,
-)
