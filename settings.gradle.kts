0cfc42d2b735bf79c9ed5809f733416293f05d19
snowykte0426 <snowykte0426@naver.com>
2026-01-11 03:06:31 +0900

    update(global): Gradle 설정 중앙화 및 빌드 최적화 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,33 @@
 rootProject.name = "datagsm-server"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
+        google()
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+        directory = file("$rootDir/.gradle/build-cache")
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
 include("datagsm-common")
 include("datagsm-authorization")
 include("datagsm-resource")
