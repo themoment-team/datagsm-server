2925b46261f0ece1ddea683ad81440c1318f3019
snowykte0426 <snowykte0426@naver.com>
2026-01-11 18:50:58 +0900

    ci/cd(global): 배포 스크립트에서 도커 정리 시점을 빌드 전으로 변경

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -7,10 +7,11 @@ cd /home/ec2-user/builds/
 echo "> Stopping and removing existing containers..." >> /home/ec2-user/deploy.log
 docker compose -f compose.prod.yaml down >> /home/ec2-user/deploy.log 2>&1
 
+echo "> Cleaning up unused Docker resources..."
+docker system prune -a --volumes -f
+
 echo "> Building and starting containers..." >> /home/ec2-user/deploy.log
 docker compose -f compose.prod.yaml up -d --build >> /home/ec2-user/deploy.log 2>&1
 
-echo "> Cleaning up unused Docker resources..."
-docker system prune -a --volumes -f
 
 echo "> Deployment completed successfully" >> /home/ec2-user/deploy.log
--- scripts/stage-deploy.sh
+++ scripts/stage-deploy.sh
@@ -7,10 +7,10 @@ cd /home/ec2-user/builds/
 echo "> Stopping and removing existing containers..."
 docker compose -f compose.stage.yaml down
 
-echo "> Building and starting containers..."
-docker compose -f compose.stage.yaml up -d --build
-
 echo "> Cleaning up unused Docker resources..."
 docker system prune -a --volumes -f
+
+echo "> Building and starting containers..."
+docker compose -f compose.stage.yaml up -d --build
 
 echo "> Deployment completed successfully"
