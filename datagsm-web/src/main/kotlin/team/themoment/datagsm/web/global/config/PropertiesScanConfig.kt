307b70c722e97b2b83d380135928bbf0ce401f7f
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-01-11 16:55:32 +0900

    fix(global): 더 이상 사용되지 않는 ConfigurationPropertiesScan 삭제

--- datagsm-web/src/main/kotlin/team/themoment/datagsm/web/global/config/PropertiesScanConfig.kt
+++ datagsm-web/src/main/kotlin/team/themoment/datagsm/web/global/config/PropertiesScanConfig.kt
@@ -1,6 +1,5 @@
 package team.themoment.datagsm.web.global.config
 
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import team.themoment.datagsm.common.global.data.ApiKeyEnvironment
@@ -8,12 +7,6 @@ import team.themoment.datagsm.common.global.data.CorsEnvironment
 import team.themoment.datagsm.common.global.data.InternalJwtEnvironment
 
 @Configuration
-@ConfigurationPropertiesScan(
-    basePackages = [
-        "team.themoment.datagsm.web.global.security.data",
-        "team.themoment.datagsm.web.global.security.jwt",
-    ],
-)
 @EnableConfigurationProperties(
     ApiKeyEnvironment::class,
     CorsEnvironment::class,
