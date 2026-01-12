8df4c4c535333c3b42a33bf1bf2dab0b72654d28
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-01-12 23:43:56 +0900

    remove(account): auth와 web 모듈에서 중복된 account api를 auth 모듈에서 삭제

--- datagsm-authorization/src/main/kotlin/team/themoment/datagsm/authorization/domain/account/service/CreateAccountService.kt
+++
@@ -1,8 +0,0 @@
-package team.themoment.datagsm.authorization.domain.account.service
-
-import team.themoment.datagsm.common.domain.account.dto.request.CreateAccountReqDto
-import team.themoment.datagsm.common.domain.account.entity.AccountJpaEntity
-
-interface CreateAccountService {
-    fun execute(reqDto: CreateAccountReqDto): AccountJpaEntity
-}
