ef2440b2f18cb7fe2416ab0763268fae85cc452d
Kim Tae Eun <snowykte0426@naver.com>
2026-01-19 03:13:23 +0900

    update(global): 중복 스케쥴링 설정 클래스 제거

--- datagsm-resource/src/main/kotlin/team/themoment/datagsm/resource/global/config/SchedulingConfig.kt
+++
@@ -1,8 +0,0 @@
-package team.themoment.datagsm.resource.global.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableScheduling
-
-@Configuration
-@EnableScheduling
-class SchedulingConfig
