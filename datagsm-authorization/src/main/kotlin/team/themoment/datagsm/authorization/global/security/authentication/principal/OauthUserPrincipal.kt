755dc0097c59ae8385c66c1e2874fd0f29d1dda4
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-01-19 20:38:33 +0900

    docs(global): Principal 클래스들에 JavaDoc 설명 추가

--- datagsm-authorization/src/main/kotlin/team/themoment/datagsm/authorization/global/security/authentication/principal/OauthUserPrincipal.kt
+++ datagsm-authorization/src/main/kotlin/team/themoment/datagsm/authorization/global/security/authentication/principal/OauthUserPrincipal.kt
@@ -2,6 +2,12 @@ package team.themoment.datagsm.authorization.global.security.authentication.prin
 
 import org.springframework.security.core.AuthenticatedPrincipal
 
+/**
+ * authorization 모듈의 OAuth2 사용자 Principal
+ * 사용자 이메일, OAuth 클라이언트 ID를 포함하며 아래 위치에서 사용됩니다.
+ * @see team.themoment.datagsm.authorization.global.security.authentication.OauthAuthenticationToken
+ * @see team.themoment.datagsm.authorization.global.security.jwt.filter.JwtAuthenticationFilter
+ */
 class OauthUserPrincipal(
     val email: String,
     val clientId: String,
--- datagsm-web/src/main/kotlin/team/themoment/datagsm/web/global/security/authentication/principal/WebUserPrincipal.kt
+++ datagsm-web/src/main/kotlin/team/themoment/datagsm/web/global/security/authentication/principal/WebUserPrincipal.kt
@@ -2,6 +2,12 @@ package team.themoment.datagsm.web.global.security.authentication.principal
 
 import org.springframework.security.core.AuthenticatedPrincipal
 
+/**
+ * web 모듈의 사용자 Principal
+ * 사용자 식별 정보(email)을 포함하며, 아래 위치에서 사용됩니다.
+ * @see team.themoment.datagsm.web.global.security.authentication.WebUserAuthenticationToken
+ * @see team.themoment.datagsm.web.global.security.jwt.filter.JwtAuthenticationFilter
+ */
 class WebUserPrincipal(
     val email: String,
 ) : AuthenticatedPrincipal {
