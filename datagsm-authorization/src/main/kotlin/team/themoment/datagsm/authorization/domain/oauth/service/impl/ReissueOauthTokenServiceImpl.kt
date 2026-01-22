c7c7d6bd4f3093f77c2f90108ccdddd7d4dfa15a
Kim Tae Eun <snowykte0426@naver.com>
2026-01-22 18:52:34 +0900

    update(oauth): KtLint 포맷팅 적용

--- datagsm-authorization/src/main/kotlin/team/themoment/datagsm/authorization/domain/oauth/service/impl/ReissueOauthTokenServiceImpl.kt
+++ datagsm-authorization/src/main/kotlin/team/themoment/datagsm/authorization/domain/oauth/service/impl/ReissueOauthTokenServiceImpl.kt
@@ -57,10 +57,12 @@ class ReissueOauthTokenServiceImpl(
                     ExpectedException("Oauth 클라이언트를 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
                 }
 
-        val scopes = client.scopes.map { scopeString ->
-            OAuthScope.fromString(scopeString)
-                ?: throw ExpectedException("Client에 유효하지 않은 권한범위가 포함되어 있습니다: $scopeString", HttpStatus.INTERNAL_SERVER_ERROR)
-        }.toSet()
+        val scopes =
+            client.scopes
+                .map { scopeString ->
+                    OAuthScope.fromString(scopeString)
+                        ?: throw ExpectedException("Client에 유효하지 않은 권한범위가 포함되어 있습니다: $scopeString", HttpStatus.INTERNAL_SERVER_ERROR)
+                }.toSet()
         val newAccessToken = jwtProvider.generateOauthAccessToken(email, account.role, clientId, scopes)
         val newRefreshToken = jwtProvider.generateOauthRefreshToken(email, clientId)
 
