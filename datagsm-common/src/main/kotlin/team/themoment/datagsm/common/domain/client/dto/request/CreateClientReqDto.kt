a70854bc6b35b88ea04255b319890f22c15a1c42
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-01-25 18:51:52 +0900

    update(client, oauth): redirectUrl 요청시 URL에 대해 유효성 검사를 진행하도록 변경

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/client/dto/request/CreateClientReqDto.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/client/dto/request/CreateClientReqDto.kt
@@ -3,6 +3,7 @@ package team.themoment.datagsm.common.domain.client.dto.request
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
+import org.hibernate.validator.constraints.URL
 
 data class CreateClientReqDto(
     @field:NotBlank
@@ -16,5 +17,9 @@ data class CreateClientReqDto(
         description = "리다이렉트 URL 목록",
         example = "[\"https://example.com/callback\", \"https://app.example.com/oauth/callback\"]",
     )
-    val redirectUrls: Set<String>,
+    val redirectUrls: Set<
+        @NotBlank(message = "Redirect URL은 필수입니다.")
+        @URL(message = "Redirect URL 형식이 올바르지 않습니다.")
+        String,
+    >,
 )
