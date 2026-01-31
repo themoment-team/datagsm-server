db413da01f17a17cbbb5be46445c2a3e64a9b233
Kim Tae Eun <snowykte0426@naver.com>
2026-01-31 20:40:39 +0900

    fix(userinfo): api key 프로퍼티 바인딩 설정 추가

--- datagsm-userinfo/src/main/kotlin/team/themoment/datagsm/userinfo/global/config/PropertiesScanConfig.kt
+++ datagsm-userinfo/src/main/kotlin/team/themoment/datagsm/userinfo/global/config/PropertiesScanConfig.kt
@@ -2,11 +2,13 @@ package team.themoment.datagsm.userinfo.global.config
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import team.themoment.datagsm.common.global.data.ApiKeyEnvironment
 import team.themoment.datagsm.common.global.data.CorsEnvironment
 import team.themoment.datagsm.common.global.data.OauthJwtEnvironment
 
 @Configuration
 @EnableConfigurationProperties(
+    ApiKeyEnvironment::class,
     CorsEnvironment::class,
     OauthJwtEnvironment::class,
 )
