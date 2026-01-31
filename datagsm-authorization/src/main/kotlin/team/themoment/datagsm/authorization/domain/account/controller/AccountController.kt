e3f16abf4b411719acbb613d8cefc34c1b00b699
Kim Tae Eun <snowykte0426@naver.com>
2026-01-31 19:32:46 +0900

    update(account): 내 정보 조회 API deprecated 처리

--- datagsm-authorization/src/main/kotlin/team/themoment/datagsm/authorization/domain/account/controller/AccountController.kt
+++ datagsm-authorization/src/main/kotlin/team/themoment/datagsm/authorization/domain/account/controller/AccountController.kt
@@ -1,5 +1,6 @@
 package team.themoment.datagsm.authorization.domain.account.controller
 
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,5 +15,13 @@ class AccountController(
     private val getMyInfoService: GetMyInfoService,
 ) {
     @GetMapping("/my")
+    @Deprecated(
+        message = "Use GET /userinfo on datagsm-userinfo (port 8083) instead",
+        replaceWith = ReplaceWith("GET http://localhost:8083/userinfo"),
+    )
+    @Operation(
+        summary = "[Deprecated] 내 정보 조회",
+        description = "새 클라이언트는 datagsm-userinfo의 /userinfo 엔드포인트를 사용하세요.",
+    )
     fun getMyInfo(): GetMyInfoResDto = getMyInfoService.execute()
 }
