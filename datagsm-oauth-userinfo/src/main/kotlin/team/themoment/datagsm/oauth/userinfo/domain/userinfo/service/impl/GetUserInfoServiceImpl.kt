becfff184cc5fd299ed3d1680e1c6b03ee0f4236
snowykte0426 <snowykte0426@naver.com>
2026-02-01 19:28:52 +0900

    fix(userinfo): 변경된 학생 엔티티 구조에 맞춰 유저 정보 조회 로직 수정

--- datagsm-oauth-userinfo/src/main/kotlin/team/themoment/datagsm/oauth/userinfo/domain/userinfo/service/impl/GetUserInfoServiceImpl.kt
+++ datagsm-oauth-userinfo/src/main/kotlin/team/themoment/datagsm/oauth/userinfo/domain/userinfo/service/impl/GetUserInfoServiceImpl.kt
@@ -5,7 +5,8 @@ import org.springframework.transaction.annotation.Transactional
 import team.themoment.datagsm.common.domain.account.dto.response.GetMyInfoResDto
 import team.themoment.datagsm.common.domain.club.dto.internal.ClubSummaryDto
 import team.themoment.datagsm.common.domain.student.dto.response.StudentResDto
-import team.themoment.datagsm.common.domain.student.entity.StudentJpaEntity
+import team.themoment.datagsm.common.domain.student.entity.BaseStudent
+import team.themoment.datagsm.common.domain.student.entity.EnrolledStudent
 import team.themoment.datagsm.oauth.userinfo.domain.userinfo.service.GetUserInfoService
 import team.themoment.datagsm.oauth.userinfo.global.security.provider.CurrentUserProvider
 
@@ -29,23 +30,45 @@ class GetUserInfoServiceImpl(
         )
     }
 
-    private fun generateStudentResDto(student: StudentJpaEntity): StudentResDto =
-        StudentResDto(
-            id = student.id!!,
-            name = student.name,
-            sex = student.sex,
-            email = student.email,
-            grade = student.studentNumber?.studentGrade,
-            classNum = student.studentNumber?.studentClass,
-            number = student.studentNumber?.studentNumber,
-            studentNumber = student.studentNumber?.fullStudentNumber,
-            major = student.major,
-            role = student.role,
-            dormitoryFloor = student.dormitoryRoomNumber?.dormitoryRoomFloor,
-            dormitoryRoom = student.dormitoryRoomNumber?.dormitoryRoomNumber,
-            isLeaveSchool = student.isLeaveSchool,
-            majorClub = student.majorClub?.let { ClubSummaryDto(id = it.id!!, name = it.name, type = it.type) },
-            jobClub = student.jobClub?.let { ClubSummaryDto(id = it.id!!, name = it.name, type = it.type) },
-            autonomousClub = student.autonomousClub?.let { ClubSummaryDto(id = it.id!!, name = it.name, type = it.type) },
-        )
+    private fun generateStudentResDto(student: BaseStudent): StudentResDto =
+        when (student) {
+            is EnrolledStudent ->
+                StudentResDto(
+                    id = student.id!!,
+                    name = student.name,
+                    sex = student.sex,
+                    email = student.email,
+                    grade = student.studentNumber.studentGrade,
+                    classNum = student.studentNumber.studentClass,
+                    number = student.studentNumber.studentNumber,
+                    studentNumber = student.studentNumber.fullStudentNumber,
+                    major = student.major,
+                    role = student.role,
+                    dormitoryFloor = student.dormitoryRoomNumber?.dormitoryRoomFloor,
+                    dormitoryRoom = student.dormitoryRoomNumber?.dormitoryRoomNumber,
+                    isLeaveSchool = student.isLeaveSchool,
+                    majorClub = student.majorClub?.let { ClubSummaryDto(id = it.id!!, name = it.name, type = it.type) },
+                    jobClub = student.jobClub?.let { ClubSummaryDto(id = it.id!!, name = it.name, type = it.type) },
+                    autonomousClub = student.autonomousClub?.let { ClubSummaryDto(id = it.id!!, name = it.name, type = it.type) },
+                )
+            else ->
+                StudentResDto(
+                    id = student.id!!,
+                    name = student.name,
+                    sex = student.sex,
+                    email = student.email,
+                    grade = null,
+                    classNum = null,
+                    number = null,
+                    studentNumber = null,
+                    major = null,
+                    role = student.role,
+                    dormitoryFloor = null,
+                    dormitoryRoom = null,
+                    isLeaveSchool = student.isLeaveSchool,
+                    majorClub = null,
+                    jobClub = null,
+                    autonomousClub = null,
+                )
+        }
 }
