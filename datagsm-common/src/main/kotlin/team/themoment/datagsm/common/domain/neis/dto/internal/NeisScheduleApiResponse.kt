b7c48e2dba833ad64dbed053252e7ac1c07e563c
snowykte0426 <snowykte0426@naver.com>
2026-02-08 06:16:20 +0900

    refactor(neis): NEIS 관련 dto의 `Jackson` 및 `Swagger` 어노테이션 대상을 field로 변경

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/neis/dto/internal/NeisScheduleApiResponse.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/neis/dto/internal/NeisScheduleApiResponse.kt
@@ -3,6 +3,6 @@ package team.themoment.datagsm.common.domain.neis.dto.internal
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class NeisScheduleApiResponse(
-    @param:JsonProperty("SchoolSchedule")
+    @field:JsonProperty("SchoolSchedule")
     val schoolSchedule: List<SchoolScheduleWrapper>?,
 )
