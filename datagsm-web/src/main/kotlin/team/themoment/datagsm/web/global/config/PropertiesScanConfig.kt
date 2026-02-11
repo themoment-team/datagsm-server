ff079bfc5137d0f25dbfdf8a51dfb44d8129a958
snowykte0426 <snowykte0426@naver.com>
2026-02-11 21:48:15 +0900

    refactor(global): ktlint 포맷팅 적용

--- datagsm-web/src/main/kotlin/team/themoment/datagsm/web/global/config/PropertiesScanConfig.kt
+++ datagsm-web/src/main/kotlin/team/themoment/datagsm/web/global/config/PropertiesScanConfig.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import team.themoment.datagsm.common.global.data.ApiKeyEnvironment
 import team.themoment.datagsm.common.global.data.CorsEnvironment
-import team.themoment.datagsm.common.global.data.EmailRateLimitEnvironment
 import team.themoment.datagsm.common.global.data.InternalJwtEnvironment
 
 @Configuration
