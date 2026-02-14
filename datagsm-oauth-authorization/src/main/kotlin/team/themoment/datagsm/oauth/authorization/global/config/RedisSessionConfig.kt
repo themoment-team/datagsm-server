a456d33192140171d91c7666b9c4907ac46c9d58
snowykte0426 <snowykte0426@naver.com>
2026-02-14 20:39:05 +0900

    update(oauth): 불필요해진 세션 관련 설정 및 구성 제거

--- datagsm-oauth-authorization/src/main/kotlin/team/themoment/datagsm/oauth/authorization/global/config/RedisSessionConfig.kt
+++
@@ -1,8 +0,0 @@
-package team.themoment.datagsm.oauth.authorization.global.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
-
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
-class RedisSessionConfig
