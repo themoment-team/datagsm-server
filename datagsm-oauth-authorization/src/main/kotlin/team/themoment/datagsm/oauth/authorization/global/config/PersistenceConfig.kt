d8465dbf66cb1fcbe502d541ee59120e897e7acc
snowykte0426 <snowykte0426@naver.com>
2026-02-16 06:31:26 +0900

    refactor(global): Repository 스캔 필터 설정 추가

--- datagsm-oauth-authorization/src/main/kotlin/team/themoment/datagsm/oauth/authorization/global/config/PersistenceConfig.kt
+++ datagsm-oauth-authorization/src/main/kotlin/team/themoment/datagsm/oauth/authorization/global/config/PersistenceConfig.kt
@@ -1,14 +1,30 @@
 package team.themoment.datagsm.oauth.authorization.global.config
 
 import org.springframework.boot.persistence.autoconfigure.EntityScan
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.FilterType
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import team.themoment.datagsm.common.domain.auth.repository.ApiKeyJpaRepository
 
 @Configuration
 @EnableJpaRepositories(
     basePackages = [
         "team.themoment.datagsm.common.domain",
+    ],
+    includeFilters = [
+        ComponentScan.Filter(
+            type = FilterType.ASSIGNABLE_TYPE,
+            classes = [JpaRepository::class],
+        ),
+    ],
+    excludeFilters = [
+        ComponentScan.Filter(
+            type = FilterType.ASSIGNABLE_TYPE,
+            classes = [ApiKeyJpaRepository::class],
+        ),
     ],
 )
 @EnableRedisRepositories(basePackages = ["team.themoment.datagsm.common.domain"])
