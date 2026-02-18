addf34970381a39722230c28558ace214f673a24
snowykte0426 <snowykte0426@naver.com>
2026-02-18 21:52:52 +0900

    refactor(neis): 급식 조회 파라미터 DTO로 변경

--- datagsm-openapi/src/main/kotlin/team/themoment/datagsm/openapi/domain/neis/meal/service/impl/SearchMealServiceImpl.kt
+++ datagsm-openapi/src/main/kotlin/team/themoment/datagsm/openapi/domain/neis/meal/service/impl/SearchMealServiceImpl.kt
@@ -1,20 +1,20 @@
 package team.themoment.datagsm.openapi.domain.neis.meal.service.impl
 
 import org.springframework.stereotype.Service
+import team.themoment.datagsm.common.domain.neis.dto.meal.request.QueryMealReqDto
 import team.themoment.datagsm.common.domain.neis.dto.meal.response.MealResDto
 import team.themoment.datagsm.common.domain.neis.meal.repository.MealRedisRepository
 import team.themoment.datagsm.openapi.domain.neis.meal.service.SearchMealService
-import java.time.LocalDate
 
 @Service
 class SearchMealServiceImpl(
     private val mealRedisRepository: MealRedisRepository,
 ) : SearchMealService {
-    override fun execute(
-        date: LocalDate?,
-        fromDate: LocalDate?,
-        toDate: LocalDate?,
-    ): List<MealResDto> {
+    override fun execute(reqDto: QueryMealReqDto): List<MealResDto> {
+        val date = reqDto.date
+        val fromDate = reqDto.fromDate
+        val toDate = reqDto.toDate
+
         val meals =
             when {
                 date != null -> mealRedisRepository.findByDate(date)
