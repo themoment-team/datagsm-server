5a455321e5f26fbb6d79f3d4e73627a1054e3bd7
snowykte0426 <snowykte0426@naver.com>
2026-02-25 15:15:20 +0900

    refactor(client): OAuth 스코프 조회 서비스 명칭을 Query로 변경 및 테스트 수정

--- datagsm-web/src/test/kotlin/team/themoment/datagsm/web/domain/client/service/QueryAvailableOauthScopesServiceTest.kt
+++ datagsm-web/src/test/kotlin/team/themoment/datagsm/web/domain/client/service/QueryAvailableOauthScopesServiceTest.kt
@@ -3,31 +3,31 @@ package team.themoment.datagsm.web.domain.client.service
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import team.themoment.datagsm.web.domain.client.service.impl.GetAvailableOauthScopesServiceImpl
+import team.themoment.datagsm.web.domain.client.service.impl.QueryAvailableOauthScopesServiceImpl
 
-class GetAvailableOauthScopesServiceTest :
+class QueryAvailableOauthScopesServiceTest :
     DescribeSpec({
 
-        val getAvailableOauthScopesService = GetAvailableOauthScopesServiceImpl()
+        val queryAvailableOauthScopesService = QueryAvailableOauthScopesServiceImpl()
 
-        describe("GetAvailableOauthScopesService 클래스의") {
+        describe("QueryAvailableOauthScopesService 클래스의") {
             describe("execute 메서드는") {
                 context("호출할 때") {
                     it("OAuthScopeGroupListResDto 형식으로 반환해야 한다") {
-                        val result = getAvailableOauthScopesService.execute()
+                        val result = queryAvailableOauthScopesService.execute()
 
                         result shouldNotBe null
                         result.list shouldNotBe null
                     }
 
                     it("비어있지 않은 리스트를 반환해야 한다") {
-                        val result = getAvailableOauthScopesService.execute()
+                        val result = queryAvailableOauthScopesService.execute()
 
                         result.list.isNotEmpty() shouldBe true
                     }
 
                     it("각 그룹의 title과 scopes가 올바르게 설정되어야 한다") {
-                        val result = getAvailableOauthScopesService.execute()
+                        val result = queryAvailableOauthScopesService.execute()
 
                         result.list.all { group ->
                             group.title.isNotBlank() && group.scopes.isNotEmpty()
@@ -35,21 +35,21 @@ class GetAvailableOauthScopesServiceTest :
                     }
 
                     it("'사용자' 카테고리가 포함되어야 한다") {
-                        val result = getAvailableOauthScopesService.execute()
+                        val result = queryAvailableOauthScopesService.execute()
 
                         val categoryTitles = result.list.map { it.title }
                         categoryTitles.contains("사용자") shouldBe true
                     }
 
                     it("'self:read' 스코프가 포함되어야 한다") {
-                        val result = getAvailableOauthScopesService.execute()
+                        val result = queryAvailableOauthScopesService.execute()
 
                         val allScopes = result.list.flatMap { it.scopes.map { scope -> scope.scope } }
                         allScopes.contains("self:read") shouldBe true
                     }
 
                     it("'self:read' 스코프의 description이 올바르게 설정되어야 한다") {
-                        val result = getAvailableOauthScopesService.execute()
+                        val result = queryAvailableOauthScopesService.execute()
 
                         val selfReadScope =
                             result.list
