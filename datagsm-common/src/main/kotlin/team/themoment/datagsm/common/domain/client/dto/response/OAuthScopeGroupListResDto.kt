0224fae29f0506f960d4a466788e31d2e00729c9
Kim Tae Eun <snowykte0426@naver.com>
2026-02-27 17:25:36 +0900

    update(client): 응답 DTO의 Swagger 어노테이션을 @field:Schema로 변경

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/client/dto/response/OAuthScopeGroupListResDto.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/client/dto/response/OAuthScopeGroupListResDto.kt
@@ -6,9 +6,9 @@ data class OAuthScopeGroupListResDto(
     val list: List<OAuthScopeGroupResDto>,
 ) {
     data class OAuthScopeGroupResDto(
-        @param:Schema(description = "카테고리 이름", example = "사용자")
+        @field:Schema(description = "카테고리 이름", example = "사용자")
         val title: String,
-        @param:Schema(description = "카테고리에 속한 OAuth 권한 범위 목록")
+        @field:Schema(description = "카테고리에 속한 OAuth 권한 범위 목록")
         val scopes: List<OAuthScopeResDto>,
     )
 }
