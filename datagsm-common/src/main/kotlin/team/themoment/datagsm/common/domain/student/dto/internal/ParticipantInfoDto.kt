c7455273cccd10c9a70a630096c197e327d27a6c
Kim Tae Eun <snowykte0426@naver.com>
2026-02-27 17:25:37 +0900

    update(student): DTO의 Swagger 어노테이션을 @field:Schema로 변경

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/student/dto/internal/ParticipantInfoDto.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/student/dto/internal/ParticipantInfoDto.kt
@@ -5,16 +5,16 @@ import team.themoment.datagsm.common.domain.student.entity.constant.Major
 import team.themoment.datagsm.common.domain.student.entity.constant.Sex
 
 data class ParticipantInfoDto(
-    @param:Schema(description = "학생 ID", example = "1")
+    @field:Schema(description = "학생 ID", example = "1")
     val id: Long,
-    @param:Schema(description = "학생 이름", example = "홍길동")
+    @field:Schema(description = "학생 이름", example = "홍길동")
     val name: String,
-    @param:Schema(description = "학생 이메일", example = "s24080@gsm.hs.kr")
+    @field:Schema(description = "학생 이메일", example = "s24080@gsm.hs.kr")
     val email: String,
-    @param:Schema(description = "학번", example = "1201")
+    @field:Schema(description = "학번", example = "1201")
     val studentNumber: Int?,
-    @param:Schema(description = "학과", example = "SW_DEVELOPMENT", allowableValues = ["SW_DEVELOPMENT", "SMART_IOT", "AI"])
+    @field:Schema(description = "학과", example = "SW_DEVELOPMENT", allowableValues = ["SW_DEVELOPMENT", "SMART_IOT", "AI"])
     val major: Major?,
-    @param:Schema(description = "성별", example = "MAN")
+    @field:Schema(description = "성별", example = "MAN")
     val sex: Sex,
 )
