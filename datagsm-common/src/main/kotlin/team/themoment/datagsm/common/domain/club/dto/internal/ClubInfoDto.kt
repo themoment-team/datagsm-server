71dbda5e48a3cbe5db5eaea3b65c297fded94592
Kim Tae Eun <snowykte0426@naver.com>
2026-02-27 17:45:33 +0900

    refactor(club): 내부 DTO에서 불필요한 validation 어노테이션 제거

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/club/dto/internal/ClubInfoDto.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/club/dto/internal/ClubInfoDto.kt
@@ -1,14 +1,11 @@
 package team.themoment.datagsm.common.domain.club.dto.internal
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
 import team.themoment.datagsm.common.domain.club.entity.constant.ClubType
 
 data class ClubInfoDto(
-    @field:NotBlank
     @field:Schema(description = "동아리명", example = "더모먼트", maxLength = 50)
     val clubName: String,
-    @field:NotBlank
     @field:Schema(description = "동아리 종류", example = "전공동아리")
     val clubType: ClubType,
     @field:Schema(description = "동아리 부장 정보", example = "2404 김태은")
