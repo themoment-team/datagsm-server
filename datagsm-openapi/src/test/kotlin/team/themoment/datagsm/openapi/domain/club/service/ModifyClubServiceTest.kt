e90d2d9d428e503b2568aebda04bf53b0f085164
hongjm0912~ <s25036@gsm.hs.kr>
2026-03-09 20:52:40 +0900

    refactor(test): 컨플릭트 해결

--- datagsm-openapi/src/test/kotlin/team/themoment/datagsm/openapi/domain/club/service/ModifyClubServiceTest.kt
+++ datagsm-openapi/src/test/kotlin/team/themoment/datagsm/openapi/domain/club/service/ModifyClubServiceTest.kt
@@ -112,9 +112,6 @@ class ModifyClubServiceTest :
                         res.id shouldBe clubId
                         res.name shouldBe "수정된동아리"
                         res.type shouldBe ClubType.MAJOR_CLUB
-                        res.leader.id shouldBe 200L
-                        res.leader.name shouldBe "새부장"
-                        res.type shouldBe ClubType.AUTONOMOUS_CLUB
                         res.leader?.id shouldBe 200L
                         res.leader?.name shouldBe "새부장"
 
