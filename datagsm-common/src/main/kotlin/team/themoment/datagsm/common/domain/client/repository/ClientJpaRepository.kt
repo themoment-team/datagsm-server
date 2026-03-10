6aaca400f3aa660a0a0ae33754d8ff38e0df4e26
snowykte0426 <snowykte0426@naver.com>
2026-03-10 10:29:09 +0900

    add(client): 계정별 클라이언트 일괄 삭제를 위한 쿼리 메서드 추가

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/client/repository/ClientJpaRepository.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/domain/client/repository/ClientJpaRepository.kt
@@ -1,9 +1,12 @@
 package team.themoment.datagsm.common.domain.client.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import team.themoment.datagsm.common.domain.account.entity.AccountJpaEntity
 import team.themoment.datagsm.common.domain.client.entity.ClientJpaEntity
 import team.themoment.datagsm.common.domain.client.repository.custom.ClientJpaCustomRepository
 
 interface ClientJpaRepository :
     JpaRepository<ClientJpaEntity, String>,
-    ClientJpaCustomRepository
+    ClientJpaCustomRepository {
+    fun deleteAllByAccount(account: AccountJpaEntity)
+}
