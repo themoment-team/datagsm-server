d6b764af3923236670321f1a9d13dcb62a010e71
snowykte0426 <snowykte0426@naver.com>
2026-03-16 16:03:28 +0900

    update(club): 동아리 조회 서비스의 트랜잭션 설정을 읽기 전용으로 변경

--- datagsm-openapi/src/main/kotlin/team/themoment/datagsm/openapi/domain/club/service/impl/QueryClubServiceImpl.kt
+++ datagsm-openapi/src/main/kotlin/team/themoment/datagsm/openapi/domain/club/service/impl/QueryClubServiceImpl.kt
@@ -15,11 +15,11 @@ import team.themoment.datagsm.common.domain.student.repository.StudentJpaReposit
 import team.themoment.datagsm.openapi.domain.club.service.QueryClubService
 
 @Service
-@Transactional
 class QueryClubServiceImpl(
     private val clubJpaRepository: ClubJpaRepository,
     private val studentJpaRepository: StudentJpaRepository,
 ) : QueryClubService {
+    @Transactional(readOnly = true)
     override fun execute(queryReq: QueryClubReqDto): ClubListResDto {
         val clubPage =
             clubJpaRepository.searchClubWithPaging(
