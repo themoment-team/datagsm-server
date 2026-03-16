465ca8f469fa39beb678ed364fca52da5a77c022
snowykte0426 <snowykte0426@naver.com>
2026-03-16 09:19:44 +0900

    update(account): 계정 권한 변경 api의 반환 타입 수정 및 공통 응답 제거

--- datagsm-web/src/main/kotlin/team/themoment/datagsm/web/domain/utility/service/ModifyAccountRoleService.kt
+++ datagsm-web/src/main/kotlin/team/themoment/datagsm/web/domain/utility/service/ModifyAccountRoleService.kt
@@ -1,11 +1,10 @@
 package team.themoment.datagsm.web.domain.utility.service
 
 import team.themoment.datagsm.common.domain.account.entity.constant.AccountRole
-import team.themoment.sdk.response.CommonApiResponse
 
 interface ModifyAccountRoleService {
     fun execute(
         email: String,
         role: AccountRole,
-    ): CommonApiResponse<Nothing>
+    )
 }
