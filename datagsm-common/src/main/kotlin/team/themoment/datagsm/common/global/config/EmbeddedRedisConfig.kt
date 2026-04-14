9db812a673c252adcf7822ae29854b11aa02815f
snowykte0426 <snowykte0426@naver.com>
2026-04-14 18:02:00 +0900

    update(global): EmbeddedRedisConfig 로거를 the-sdk logger()로 교체 및 !! 연산자 제거

--- datagsm-common/src/main/kotlin/team/themoment/datagsm/common/global/config/EmbeddedRedisConfig.kt
+++ datagsm-common/src/main/kotlin/team/themoment/datagsm/common/global/config/EmbeddedRedisConfig.kt
@@ -1,24 +1,24 @@
 package team.themoment.datagsm.common.global.config
 
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.DisposableBean
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import redis.embedded.RedisServer
+import team.themoment.sdk.logging.logger.logger
 
 @Configuration
 @Profile("stage")
 class EmbeddedRedisConfig :
     InitializingBean,
     DisposableBean {
-    private val log = LoggerFactory.getLogger(EmbeddedRedisConfig::class.java)
     private var redisServer: RedisServer? = null
 
     override fun afterPropertiesSet() {
-        log.info("Starting embedded Redis server on port 6379")
-        redisServer = RedisServer(6379)
-        redisServer!!.start()
+        logger().info("Starting embedded Redis server on port 6379")
+        val server = RedisServer(6379)
+        server.start()
+        redisServer = server
     }
 
     override fun destroy() {
